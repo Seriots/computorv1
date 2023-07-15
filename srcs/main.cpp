51bdce9bc00746502e00c8bba56c4b10a01a62d9
seriots <leo.giband@gmail.com>
2023-07-15 15:00:41 +0200

    RemoveComment

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,10 +3,9 @@
 #include "Polynome.hpp"
 
 /* Second degrees polynome resolution
- * 1. Input Parsing with token: number-operator
+ * 1. Input Parsing with token: number-operator OK
  * 2. Simplification OK
  * 3. Resolution OK
- * 4. Add a Prime number parser and a fraction reducer
  */
 int main(int argc, char **argv)
 {
